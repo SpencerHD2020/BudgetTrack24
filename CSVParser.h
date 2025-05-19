226bfdde6677444b022b1517a325b79d0c6a6db2
Spencer Nowlin <spencernowlin@icloud.com>
2025-05-19 15:17:00 -0500

    Added Totals Struct
    Should probably rename CSVParser at somepoint to like DataInterface or something like that

--- CSVParser.h
+++ CSVParser.h
@@ -28,6 +28,32 @@ namespace CSV
         QString Desc;
     };
 
+
+    // Ideally we should have totals stored in a CSV Somewhere to reference past totals, also would like to cache them so we don't have to recalculate them
+    //      each time the View comes back up
+
+    // Though we likely will only have 1-2 instances of this struct floating around at a given time (past [when building new] and cached current [write to CSV on calc]])
+
+    struct Totals
+    {
+        Totals()
+        {}
+
+        Totals(QString rawTotal /* Full balance with no deductions */, QString totalBills, QString totalDebt /* CC Total */, QString totalExtra /* rawTotal - (bills + CC) */)
+            : RawTotal(rawTotal)
+            , TotalBills(totalBills)
+            , TotalDebt(totalDebt)
+            , TotalExtra(totalExtra)
+        {}
+
+        QString RawTotal;
+        QString TotalBills;
+        QString TotalDebt;
+        QString TotalExtra;
+    };
+
+
+
     class CSVParser : public QObject
     {
         Q_OBJECT
